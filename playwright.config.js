cd3cb8546446ffab51b29791a206329e38298a14
Keith Hart <keith@ownimage.co.uk>
2026-07-29 18:10:52 +0100

    The progress badge is the primary button colour.  This can be confusing can you change it to a different colour

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: '100%',
+  workers: 4,
   retries: 2,
   reporter: "html",
   use: {
